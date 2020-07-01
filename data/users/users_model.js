2a0145e7b9256182b68a9784626bfdec115a77c0
roselandroche <r.landroche@gmail.com>
2020-07-01 19:33:09 -0400

    Wrote out users tests, and model

--- data/users/users_model.js
+++ data/users/users_model.js
@@ -1,11 +1,33 @@
+const bcrypt = require('bcryptjs')
 const db = require('../db_config')
 
-// find
+function find() {
+    return db('users')
+        .select('username', 'email')
+}
 
-// findBy
+async function add(user) {
+    user.password = await bcrypt.hash(user.password, 13)
+    return db('users')
+        .insert(user)
+        .returning('*')
+}
 
-// add
+function findById(id) {
+    return db('users')
+        .where({ id })
+        .first()
+}
+
+function findBy(filter) {
+    return db('users')
+        .where( filter )
+        .first()
+}
 
 module.exports = {
-
+    find,
+    add,
+    findById,
+    findBy
 }
--- data/users/users_router.spec.js
+++ data/users/users_router.spec.js
@@ -2,10 +2,43 @@ const supertest = ('supertest');
 const server = ('../api/server.js');
 const db = require('../db_config');
 
-// beforeAll(async () => {
-
-// })
+beforeAll(async () => {
+    console.log('Called tests on USERS')
+    await db.truncate('users')
+    await db.seed.run()
+})
 
 describe('users router', () => {
-    
+    describe('register', () => {
+        it('should register a new user, return 201', async () => {
+            const res = await supertest(server).post('/register').send({
+                username: 'Sean Johnson',
+                password: 'abc',
+                email: 's.johnson@company.com'
+            })
+            expect(res.status).toBe(201)
+            expect(res.type).toBe('application/json')
+            expect(res.username).toBe('Sean Johnson')
+        })
+        it('should NOT register duplicate user, return 400', async () => {
+            const res = await supertest(server).post('/register').send({
+                username: 'Rose Smith',
+                password: 'abc',
+                email: 'rose.smith@company.com'
+            })
+            expect(res.status).toBe(400)
+            expect(res.type).toBe('application/json')
+            expect(res.message).toBe('User already exists')
+        })
+        it('should NOT register duplicate email, return 400', async () => {
+            const res = await supertest(server).post('/register').send({
+                username: 'Chrissy Taylor',
+                password: '123',
+                email: 'r.smith@company.com'
+            })
+            expect(res.status).toBe(400)
+            expect(res.type).toBe('application/json')
+            expect(res.message).toBe('Email already exists')
+        })
+    })
 })
